74801adb251e0afd5965041ee552c47af0eb6abf
Jose Luis Duran <jlduran@users.noreply.github.com>
2014-10-04 09:22:40 -0500

    Fix units

--- app/controllers/admin/units_controller.rb
+++ app/controllers/admin/units_controller.rb
@@ -2,43 +2,49 @@ class Admin::UnitsController < Admin::ApplicationController
   def index
     @units = Unit.all
   end
-  
+
   def show
     @unit = Unit.find(params[:id])
   end
-  
+
   def new
     @unit = Unit.new
   end
-  
+
   def create
-    @unit = Unit.new(params[:unit])
+    @unit = Unit.new(unit_params)
     if @unit.save
-      flash[:notice] = "Successfully created unit."
-      redirect_to [:admin, @unit], :only_path => true
+      flash[:notice] = 'Successfully created unit.'
+      redirect_to [:admin, @unit]
     else
-      render :action => 'new'
+      render action: 'new'
     end
   end
-  
+
   def edit
     @unit = Unit.find(params[:id])
   end
-  
+
   def update
     @unit = Unit.find(params[:id])
-    if @unit.update_attributes(params[:unit])
-      flash[:notice] = "Successfully updated unit."
-      redirect_to [:admin, @unit], :only_path => true
+    if @unit.update(unit_params)
+      flash[:notice] = 'Successfully updated unit.'
+      redirect_to [:admin, @unit]
     else
-      render :action => 'edit'
+      render action: 'edit'
     end
   end
-  
+
   def destroy
     @unit = Unit.find(params[:id])
     @unit.destroy
-    flash[:notice] = "Successfully destroyed unit."
+    flash[:notice] = 'Successfully destroyed unit.'
     redirect_to admin_units_url
+  end
+
+  private
+
+  def unit_params
+    params.require(:unit).permit(:name)
   end
 end
