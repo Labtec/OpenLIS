88913cec58b2e965097a13e8503a3919f276112d
Jose Luis Duran <jlduran@users.noreply.github.com>
2014-10-03 21:09:56 -0300

    Fix doctors

--- app/controllers/admin/doctors_controller.rb
+++ app/controllers/admin/doctors_controller.rb
@@ -2,17 +2,17 @@ class Admin::DoctorsController < Admin::ApplicationController
   def index
     @doctors = Doctor.all
   end
-  
+
   def show
     @doctor = Doctor.find(params[:id])
   end
-  
+
   def new
     @doctor = Doctor.new
   end
-  
+
   def create
-    @doctor = Doctor.new(params[:doctor])
+    @doctor = Doctor.new(doctor_params)
     if @doctor.save
       flash[:notice] = "Successfully created doctor."
       redirect_to [:admin, @doctor], :only_path => true
@@ -20,25 +20,31 @@ class Admin::DoctorsController < Admin::ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def edit
     @doctor = Doctor.find(params[:id])
   end
-  
+
   def update
     @doctor = Doctor.find(params[:id])
-    if @doctor.update_attributes(params[:doctor])
+    if @doctor.update_attributes(doctor_params)
       flash[:notice] = "Successfully updated doctor."
       redirect_to [:admin, @doctor], :only_path => true
     else
       render :action => 'edit'
     end
   end
-  
+
   def destroy
     @doctor = Doctor.find(params[:id])
     @doctor.destroy
     flash[:notice] = "Successfully deleted doctor."
     redirect_to admin_doctors_url
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name)
   end
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,16 +1,15 @@
 class Doctor < ActiveRecord::Base
-  attr_accessible :name
   has_many :accessions, :dependent => :nullify
-
-  default_scope :order => :name
 
   validates_presence_of :name
   validates_uniqueness_of :name
 
   before_save :purge_trailing_spaces
 
-  named_scope :limit, lambda { |limit| {:limit => limit} }
-  named_scope :search_for_name, lambda { |term| {:conditions => ['lower(name) LIKE ?', "%#{term.try(:downcase)}%"]} }
+  #scope :limit, lambda { |limit| {:limit => limit} }
+  #scope :search_for_name, lambda { |term| {:conditions => ['lower(name) LIKE ?', "%#{term.try(:downcase)}%"]} }
+
+  default_scope { order(name: :asc) }
 
 private
 
