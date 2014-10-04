217d5220aae213eadfcc44d0c7b8f6b98846015a
Jose Luis Duran <jlduran@users.noreply.github.com>
2014-10-03 21:17:55 -0300

    Fix insurance providers

--- app/models/insurance_provider.rb
+++ app/models/insurance_provider.rb
@@ -4,11 +4,11 @@ class InsuranceProvider < ActiveRecord::Base
   has_many :patients
   has_many :prices, through: :price_list
   has_many :claims
-  
+
   def submitted_claims
     claims.submitted
   end
-  
+
   def unsubmitted_claims
     unsubmitted_claims = []
     all_unsubmitted_claims = Accession.find(Accession.with_insurance_provider.map(&:id) - Claim.submitted.map(&:accession_id), include: :patient)
