819e642b0e986e530f10260445fdbc636658fd7a
Jose Luis Duran <jlduran@users.noreply.github.com>
2014-10-03 21:31:44 -0300

    Fix departments

--- app/controllers/admin/departments_controller.rb
+++ app/controllers/admin/departments_controller.rb
@@ -1,17 +1,16 @@
 class Admin::DepartmentsController < Admin::ApplicationController
   def index
-    @departments = Department.all(include: :lab_tests)
+    @departments = Department.all #.include(:lab_tests)
   end
 
   def new
     @department = Department.new
-    render layout: false
   end
 
   def create
     @department = Department.new(department_params)
     if @department.save
-      flash[:notice] = "Department created successfully."
+      flash[:notice] = 'Department created successfully.'
       redirect_to admin_departments_url
     else
       render action: 'new'
@@ -25,7 +24,7 @@ class Admin::DepartmentsController < Admin::ApplicationController
   def update
     @department = Department.find(params[:id])
     if @department.update_attributes(department_params)
-      flash[:notice] = "Department updated successfully."
+      flash[:notice] = 'Department updated successfully.'
       redirect_to admin_departments_url
     else
       render action: 'edit'
@@ -35,7 +34,7 @@ class Admin::DepartmentsController < Admin::ApplicationController
   def destroy
     @department = Department.find(params[:id])
     @department.destroy
-    flash[:notice] = "Department deleted."
+    flash[:notice] = 'Department deleted.'
     redirect_to admin_departments_url
   end
 
--- app/models/department.rb
+++ app/models/department.rb
@@ -2,6 +2,8 @@ class Department < ActiveRecord::Base
   # Consider caching this model
   has_many :lab_tests, dependent: :destroy
   has_many :notes, dependent: :destroy
-  validates_presence_of :name
-  validates_uniqueness_of :name
+
+  validates :name,
+    presence:   true,
+    uniqueness: true
 end
