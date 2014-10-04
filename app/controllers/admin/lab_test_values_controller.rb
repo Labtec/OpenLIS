e771b928120fcdca0c19ae8768944c77a4e6ef79
Jose Luis Duran <jlduran@users.noreply.github.com>
2014-10-04 09:33:04 -0500

    Fix lab test values

--- app/controllers/admin/lab_test_values_controller.rb
+++ app/controllers/admin/lab_test_values_controller.rb
@@ -1,44 +1,50 @@
 class Admin::LabTestValuesController < Admin::ApplicationController
   def index
-    @lab_test_values = LabTestValue.all(:order => "value ASC")
+    @lab_test_values = LabTestValue.all.order(value: :asc)
   end
-  
+
   def show
     @lab_test_value = LabTestValue.find(params[:id])
   end
-  
+
   def new
     @lab_test_value = LabTestValue.new
   end
-  
+
   def create
-    @lab_test_value = LabTestValue.new(params[:lab_test_value])
+    @lab_test_value = LabTestValue.new(lab_test_value_params)
     if @lab_test_value.save
-      flash[:notice] = "Successfully created lab test value."
-      redirect_to [:admin, @lab_test_value], :only_path => true
+      flash[:notice] = 'Successfully created lab test value.'
+      redirect_to [:admin, @lab_test_value]
     else
-      render :action => 'new'
+      render action: 'new'
     end
   end
-  
+
   def edit
     @lab_test_value = LabTestValue.find(params[:id])
   end
-  
+
   def update
     @lab_test_value = LabTestValue.find(params[:id])
-    if @lab_test_value.update_attributes(params[:lab_test_value])
-      flash[:notice] = "Successfully updated lab test value."
-      redirect_to [:admin, @lab_test_value], :only_path => true
+    if @lab_test_value.update(lab_test_value_params)
+      flash[:notice] = 'Successfully updated lab test value.'
+      redirect_to [:admin, @lab_test_value]
     else
-      render :action => 'edit'
+      render action: 'edit'
     end
   end
-  
+
   def destroy
     @lab_test_value = LabTestValue.find(params[:id])
     @lab_test_value.destroy
-    flash[:notice] = "Successfully destroyed lab test value."
+    flash[:notice] = 'Successfully destroyed lab test value.'
     redirect_to admin_lab_test_values_url
+  end
+
+  private
+
+  def lab_test_value_params
+    params.require(:lab_test_value).permit(:value, :flag, :note)
   end
 end
