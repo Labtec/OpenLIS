3806f250a3239af3de775f453dd97173285c12b3
Jose Luis Duran <jlduran@users.noreply.github.com>
2014-10-15 00:16:44 -0500

    Modify config parameters

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,2 +1,17 @@
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :password_confirmation, :given_name, :middle_name, :family_name, :family_name2, :birthdate, :identifier, :email, :phone, :address, :animal_type, :insurance_provider_id, :search]
+Rails.application.config.filter_parameters += [
+  :address,
+  :animal_type,
+  :birthdate,
+  :email,
+  :family_name,
+  :family_name2,
+  :given_name,
+  :identifier,
+  :insurance_provider_id,
+  :middle_name,
+  :password,
+  :password_confirmation,
+  :phone,
+  :search
+]
