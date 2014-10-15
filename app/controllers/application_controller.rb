cbeaae3648d885dea5f863823d240603ca7933e9
Jose Luis Duran <jlduran@users.noreply.github.com>
2014-10-14 21:46:31 -0500

    Set language before authenticating the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!
   before_filter :set_user_language
+  before_filter :authenticate_user!
   before_filter :set_active_tab
 
 protected
