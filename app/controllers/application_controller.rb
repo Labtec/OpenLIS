cf970818deb55b05fff81734f82527ba66635f6b
Jose Luis Duran <jlduran@users.noreply.github.com>
2015-01-10 09:28:16 -0200

    Fix /logout 401 bug
    See https://github.com/plataformatec/devise/wiki/How-To:-Change-the-redirect-path-after-destroying-a-session-i.e.-signing-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,9 @@ protected
       I18n.locale = "es"
     end
   end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
