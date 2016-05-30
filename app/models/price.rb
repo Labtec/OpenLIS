24ab4db1755888a6f46078d0176e15b756df20b3
Jose Luis Duran <jlduran@users.noreply.github.com>
2016-05-30 02:03:29 -0300

    Simplify positive amount validation

--- app/models/price.rb
+++ app/models/price.rb
@@ -4,15 +4,8 @@ class Price < ApplicationRecord
   belongs_to :price_list, inverse_of: :prices
   belongs_to :priceable, polymorphic: true
 
-  validates :amount, numericality: true
+  validates :amount, numericality: { greater_than_or_equal_to: 0.00 }
   validates :price_list_id, presence: true
   validates :priceable_id, presence: true, uniqueness: { scope: :price_list_id }
   validates :priceable_type, presence: true
-  validate :positive_amount
-
-  private
-
-  def positive_amount
-    errors.add(:amount, 'should be at least 0.00') if amount.nil? || amount < 0.00
-  end
 end
