c0234849ebe84b95539ca1b0d12f117c866e9020
Jose Luis Duran <jlduran@users.noreply.github.com>
2017-06-26 23:15:49 -0300

    RuboCop: Style/Lambda

--- app/models/reference_range.rb
+++ app/models/reference_range.rb
@@ -23,9 +23,9 @@ class ReferenceRange < ApplicationRecord
 
   scope :for_its_gender, ->(gender) { where(gender: [gender, '*']) }
   scope :for_its_type, ->(type) { where(animal_type: [type, 0]) }
-  scope :for_its_age_in_units, ->(age_days, age_weeks, age_months, age_years) {
+  scope :for_its_age_in_units, (lambda do |age_days, age_weeks, age_months, age_years|
     where(["(min_age <= ? AND max_age > ? AND age_unit = 'D') OR (min_age <= ? AND max_age IS NULL AND age_unit = 'D') OR (min_age IS NULL AND max_age > ? AND age_unit = 'D') OR (min_age IS NULL AND max_age IS NULL) OR (min_age <= ? AND max_age > ? AND age_unit = 'W') OR (min_age <= ? AND max_age IS NULL AND age_unit = 'W') OR (min_age IS NULL AND max_age > ? AND age_unit = 'W') OR (min_age IS NULL AND max_age IS NULL) OR (min_age <= ? AND max_age > ? AND age_unit = 'M') OR (min_age <= ? AND max_age IS NULL AND age_unit = 'M') OR (min_age IS NULL AND max_age > ? AND age_unit = 'M') OR (min_age IS NULL AND max_age IS NULL) OR (min_age <= ? AND max_age > ? AND age_unit = 'Y') OR (min_age <= ? AND max_age IS NULL AND age_unit = 'Y') OR (min_age IS NULL AND max_age > ? AND age_unit = 'Y') OR (min_age IS NULL AND max_age IS NULL)", age_days, age_days, age_days, age_days, age_weeks, age_weeks, age_weeks, age_weeks, age_months, age_months, age_months, age_months, age_years, age_years, age_years, age_years])
-  }
+  end)
 
   validates :animal_type, inclusion: { in: ANIMAL_SPECIES }, allow_blank: true
   validates :gender, inclusion: { in: GENDERS.map { |_disp, value| value } }
