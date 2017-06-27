ce3410e06d34e16da8b1e3611063251ae83a79b7
Jose Luis Duran <jlduran@users.noreply.github.com>
2017-06-26 23:46:42 -0300

    RuboCop: Style/ClassAndModuleChildren

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,29 +6,35 @@ require 'rails/test_help'
 require 'capybara/rails'
 require 'bcrypt'
 
-class ActiveSupport::TestCase
-  fixtures :all
-end
-
-class ActionDispatch::IntegrationTest
-  include Capybara::DSL
-  include Warden::Test::Helpers
-
-  Warden.test_mode!
-
-  fixtures :users
-
-  def select_date(date, options = {})
-    raise ArgumentError, 'from is a required option' if options[:from].blank?
-    field = options[:from].to_s
-    select date.year.to_s,               from: "#{field}_1i"
-    select Date::MONTHNAMES[date.month], from: "#{field}_2i"
-    select date.day.to_s,                from: "#{field}_3i"
+module ActiveSupport
+  class TestCase
+    fixtures :all
   end
 end
 
-class ActionController::TestCase
-  include Devise::Test::ControllerHelpers
+module ActionDispatch
+  class IntegrationTest
+    include Capybara::DSL
+    include Warden::Test::Helpers
+
+    Warden.test_mode!
+
+    fixtures :users
+
+    def select_date(date, options = {})
+      raise ArgumentError, 'from is a required option' if options[:from].blank?
+      field = options[:from].to_s
+      select date.year.to_s,               from: "#{field}_1i"
+      select Date::MONTHNAMES[date.month], from: "#{field}_2i"
+      select date.day.to_s,                from: "#{field}_3i"
+    end
+  end
+end
+
+module ActionController
+  class TestCase
+    include Devise::Test::ControllerHelpers
+  end
 end
 
 Shoulda::Matchers.configure do |config|
