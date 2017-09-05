37cfe2876406389327dea7a8f220bc609ae3a166
Jose Luis Duran <jlduran@users.noreply.github.com>
2017-09-05 03:26:44 -0300

    Indicate preliminary results in email subject

--- app/mailers/results_mailer.rb
+++ app/mailers/results_mailer.rb
@@ -3,15 +3,17 @@
 class ResultsMailer < ApplicationMailer
   def email_doctor(accession, pdf)
     @accession = accession
+    subject = @accession.reported_at ? '.subject' : '.subject_preliminary'
     attachments["resultados_#{@accession.id}.pdf"] = pdf.render if pdf
     mail(to: @accession.doctor.email,
-         subject: t('.subject', full_name: full_name(@accession.patient)))
+         subject: t(subject, full_name: full_name(@accession.patient)))
   end
 
   def email_patient(accession, pdf)
     @accession = accession
+    subject = @accession.reported_at ? '.subject' : '.subject_preliminary'
     attachments["resultados_#{@accession.id}.pdf"] = pdf.render if pdf
     mail(to: @accession.patient.email,
-         subject: t('.subject'))
+         subject: t(subject))
   end
 end
