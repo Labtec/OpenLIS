234cb98ea2989963dcf3748f77a667d86773c561
Jose Luis Duran <jlduran@users.noreply.github.com>
2018-04-13 11:06:40 -0300

    Refactor CSP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,6 +66,6 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true
-    Bullet.rails_logger = true
+    Bullet.add_footer = true
   end
 end
