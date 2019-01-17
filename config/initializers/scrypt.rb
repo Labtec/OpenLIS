0608d114b2efe3eb3837c97a35ecd9c2f658c320
Jose Luis Duran <jlduran@users.noreply.github.com>
2019-01-16 23:58:11 -0200

    Revert this commit once scrypt migration is over

--- config/initializers/scrypt.rb
+++ config/initializers/scrypt.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'bcrypt'
 require 'scrypt'
 
 module Devise
@@ -12,7 +13,19 @@ module Devise
     def self.compare(klass, hashed_password, password)
       return false if hashed_password.blank?
 
+      original_password = password
       password = "#{password}#{klass.pepper}" if klass.pepper.present?
+
+      if hashed_password.start_with?('$2a$')
+        bcrypt   = ::BCrypt::Password.new(hashed_password)
+        password = ::BCrypt::Engine.hash_secret(password, bcrypt.salt)
+        if Devise.secure_compare(password, hashed_password)
+          User.find_by(encrypted_password: hashed_password)
+              .update!(encrypted_password: digest(klass, original_password))
+          return true
+        end
+      end
+
       scrypt   = ::SCrypt::Password.new(hashed_password)
       salt     = "#{scrypt.cost}#{scrypt.salt}"
       password = ::SCrypt::Engine.hash_secret(password, salt)
