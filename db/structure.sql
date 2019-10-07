41b4ae14a7c705007c81b56ffbd0910c694868c9
Jose Luis Duran <jlduran@users.noreply.github.com>
2019-10-07 09:15:51 -0300

    Update to PostgreSQL 12

--- db/structure.sql
+++ db/structure.sql
@@ -5,6 +5,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -45,7 +46,7 @@ CREATE SEQUENCE public.accession_panels_id_seq
 
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
 
 --
 -- Name: accession_panels; Type: TABLE; Schema: public; Owner: -
