5d857ca57a7c4dce82b7692126705d29826666af
Jose Luis Duran <jlduran@users.noreply.github.com>
2020-06-09 08:11:31 -0300

    Use URI's MailTo EMAIL_REGEXP
    Regexp:
    
        /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -3,6 +3,6 @@
 # Validates email format
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || I18n.t(:invalid, scope: %i[errors messages])) unless value.match?(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i)
+    record.errors[attribute] << (options[:message] || I18n.t(:invalid, scope: %i[errors messages])) unless URI::MailTo::EMAIL_REGEXP.match?(value)
   end
 end
