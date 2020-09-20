b3f8ab8f2ae13f70f5a0fc69b67a9e501566f4a7
Jose Luis Duran <jlduran@users.noreply.github.com>
2020-09-20 19:24:13 -0300

    Unit: Allow blank names backed by UCUM
    For example, a pH observation has no unit, however, when transmitting
    the value, a valid UCUM code ([pH]) shall be set.

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -3,9 +3,15 @@
 class Unit < ApplicationRecord
   has_many :lab_tests, dependent: :nullify
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, unless: -> { ucum.present? }
 
   default_scope { order(name: :asc) }
 
-  auto_strip_attributes :name
+  auto_strip_attributes :name, :ucum
+
+  def name_display
+    return name if name.present?
+
+    ucum
+  end
 end
