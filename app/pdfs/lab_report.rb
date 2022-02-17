Labtec/OpenLIS

commit aa8ae98cb180566fc75439958b7ff711410b384b

Format observations valueQuantity with comparator
Format accordingly when an observation's valueQuantity contains a
comparator.

app/pdfs/lab_report.rb
@@ -191,10 +191,10 @@ class LabReport < Prawn::Document
 
         if result.normal?
           cell_col0 = make_cell content: result.lab_test_name, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
-          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt; ').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
+          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt;').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
         else
           cell_col0 = make_cell content: result.lab_test_name, background_color: colors_highlight_gray, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
-          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt; ').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), background_color: colors_highlight_gray, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
+          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt;').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), background_color: colors_highlight_gray, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
         end
         cell_col2 = make_cell content: display_format_units(result), padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
         cell_col3 = make_cell content: flag_name(result), font_style: :bold, text_color: flag_color[flag_color(result).to_sym], padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]

app/helpers/observations_helper.rb
@@ -31,8 +31,19 @@ module ObservationsHelper
     elsif observation.lab_test.text_length?
       observation.value
     else
-      number_with_precision(observation.value, precision: observation.lab_test_decimals, delimiter: ',')
+      if observation_value_comparator(observation.value).present?
+        [
+          observation_value_comparator(observation.value),
+          number_with_precision(observation.value.scan(/[\d.]/).join, precision: observation.lab_test_decimals, delimiter: ',')
+        ].join
+      else
+        number_with_precision(observation.value, precision: observation.lab_test_decimals, delimiter: ',')
+      end
     end
+  end
+
+  def observation_value_comparator(value)
+    value.scan(/[^\d.]/).join.squish
   end
 
   def format_units(observation)

test/helpers/observations_helper_test.rb
@@ -23,6 +23,12 @@ class ObservationsHelperTest < ActionView::TestCase
 
     @observation.lab_test = lab_tests(:decimal)
     assert_equal '1,000.0', format_value(@observation)
+
+    @observation.value = '> 1000'
+    assert_equal '>1,000.0', format_value(@observation)
+
+    @observation.value = '>1000'
+    assert_equal '>1,000.0', format_value(@observation)
   end
 
   test 'format qualitative observation' do

commit d52ae763ad91348c8c6d2b9a6cb6d824169723bd

Fix a display regression
This regression was introduced in aa8ae98.

app/pdfs/lab_report.rb
@@ -191,10 +191,10 @@ class LabReport < Prawn::Document
 
         if result.normal?
           cell_col0 = make_cell content: result.lab_test_name, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
-          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt;').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
+          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt; ').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
         else
           cell_col0 = make_cell content: result.lab_test_name, background_color: colors_highlight_gray, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
-          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt;').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), background_color: colors_highlight_gray, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
+          cell_col1 = make_cell content: format_value(result).gsub(/</, '&lt; ').gsub(/&lt; i/, '<i').gsub(/&lt; s/, '<s').gsub(%r{&lt; /}, '</'), background_color: colors_highlight_gray, inline_format: true, padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
         end
         cell_col2 = make_cell content: display_format_units(result), padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]
         cell_col3 = make_cell content: flag_name(result), font_style: :bold, text_color: flag_color[flag_color(result).to_sym], padding: [ROW_VERTICAL_PADDING, PADDING, ROW_VERTICAL_PADDING, PADDING]