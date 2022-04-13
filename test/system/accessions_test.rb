3da34b7fa534eb51bc170b1224aa72d75cc0ba4a
Jose Luis Duran <jlduran@users.noreply.github.com>
2022-04-13 12:22:40 -0300

    Test all with a single command
    Further randomizes testing.

--- test/system/accessions_test.rb
+++ test/system/accessions_test.rb
@@ -2,57 +2,59 @@
 
 require 'application_system_test_case'
 
-class AccessionsTest < ApplicationSystemTestCase
-  def setup
-    login_as(users(:user), scope: :user)
-    @patient = patients(:john)
-  end
+module System
+  class AccessionsTest < ApplicationSystemTestCase
+    def setup
+      login_as(users(:user), scope: :user)
+      @patient = patients(:john)
+    end
 
-  def teardown
-    Warden.test_reset!
-  end
+    def teardown
+      Warden.test_reset!
+    end
 
-  test 'create new order from panel' do
-    visit patient_url(@patient)
-    within('.title_tools') { click_on 'Order Tests' }
-    within('#order_tests') { click_on 'Chemistry' }
+    test 'create new order from panel' do
+      visit patient_url(@patient)
+      within('.title_tools') { click_on 'Order Tests' }
+      within('#order_tests') { click_on 'Chemistry' }
 
-    assert page.has_unchecked_field?('BUN')
-    assert page.has_unchecked_field?('Cholesterol')
+      assert page.has_unchecked_field?('BUN')
+      assert page.has_unchecked_field?('Cholesterol')
 
-    check 'Panel'
+      check 'Panel'
 
-    assert page.has_field?('BUN', disabled: true), 'BUN is not disabled'
-    assert page.has_field?('Cholesterol', disabled: true), 'CHOL is not disabled'
+      assert page.has_field?('BUN', disabled: true), 'BUN is not disabled'
+      assert page.has_field?('Cholesterol', disabled: true), 'CHOL is not disabled'
 
-    click_on 'Save'
+      click_on 'Save'
 
-    assert_not page.has_content?('error'), 'Requisition not created'
-    assert page.has_content?('Successfully created requisition')
-    assert page.has_content?('BUN')
-    assert page.has_content?('Cholesterol')
-  end
+      assert_not page.has_content?('error'), 'Requisition not created'
+      assert page.has_content?('Successfully created requisition')
+      assert page.has_content?('BUN')
+      assert page.has_content?('Cholesterol')
+    end
 
-  test 'edit a paneled order' do
-    accession = accessions(:paneled)
-    visit edit_accession_url(accession)
+    test 'edit a paneled order' do
+      accession = accessions(:paneled)
+      visit edit_accession_url(accession)
 
-    assert page.has_checked_field?('Panel'), 'Panel is not checked'
+      assert page.has_checked_field?('Panel'), 'Panel is not checked'
 
-    within('#order_tests') { click_on 'Chemistry' }
-    assert page.has_field?('BUN', disabled: true), 'BUN is not disabled'
-    assert page.has_field?('Cholesterol', disabled: true), 'CHOL is not disabled'
+      within('#order_tests') { click_on 'Chemistry' }
+      assert page.has_field?('BUN', disabled: true), 'BUN is not disabled'
+      assert page.has_field?('Cholesterol', disabled: true), 'CHOL is not disabled'
 
-    uncheck 'Panel'
+      uncheck 'Panel'
 
-    assert page.has_checked_field?('BUN'), 'BUN is not checked'
-    assert page.has_checked_field?('Cholesterol'), 'CHOL is not checked'
+      assert page.has_checked_field?('BUN'), 'BUN is not checked'
+      assert page.has_checked_field?('Cholesterol'), 'CHOL is not checked'
 
-    click_on 'Save'
+      click_on 'Save'
 
-    assert_not page.has_content?('error'), 'Requisition not created'
-    assert page.has_content?('Successfully updated requisition')
-    assert page.has_content?('BUN')
-    assert page.has_content?('Cholesterol')
+      assert_not page.has_content?('error'), 'Requisition not created'
+      assert page.has_content?('Successfully updated requisition')
+      assert page.has_content?('BUN')
+      assert page.has_content?('Cholesterol')
+    end
   end
 end
--- test/system/patients_test.rb
+++ test/system/patients_test.rb
@@ -2,32 +2,34 @@
 
 require 'application_system_test_case'
 
-class PatientsTest < ApplicationSystemTestCase
-  def setup
-    login_as(users(:user), scope: :user)
-  end
+module System
+  class PatientsTest < ApplicationSystemTestCase
+    def setup
+      login_as(users(:user), scope: :user)
+    end
 
-  def teardown
-    Warden.test_reset!
-  end
+    def teardown
+      Warden.test_reset!
+    end
 
-  test 'create new patient' do
-    visit new_patient_url
-    fill_in 'Given name', with: 'Amber'
-    fill_in 'Last name', with: 'Zigbee'
-    select 'Female', from: 'Sex'
-    select_date 25.years.ago, from: 'patient_birthdate'
-    click_on 'Save Patient'
+    test 'create new patient' do
+      visit new_patient_url
+      fill_in 'Given name', with: 'Amber'
+      fill_in 'Last name', with: 'Zigbee'
+      select 'Female', from: 'Sex'
+      select_date 25.years.ago, from: 'patient_birthdate'
+      click_on 'Save Patient'
 
-    assert_not page.has_content?('error'), 'New patient not added'
-    assert page.has_content?('Amber Zigbee')
-  end
+      assert_not page.has_content?('error'), 'New patient not added'
+      assert page.has_content?('Amber Zigbee')
+    end
 
-  test 'search for a patient' do
-    visit root_path
-    fill_in 'Search patients', with: "Alicia Doe\n"
+    test 'search for a patient' do
+      visit root_path
+      fill_in 'Search patients', with: "Alicia Doe\n"
 
-    assert_not page.has_content?('222-222-2222'), 'Non-searched patient'
-    assert page.has_content?('111-111-1111'), 'Patient search failed'
+      assert_not page.has_content?('222-222-2222'), 'Non-searched patient'
+      assert page.has_content?('111-111-1111'), 'Patient search failed'
+    end
   end
 end
