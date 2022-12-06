2765d8d59c0d8a81dcee6682611040f4e6c39a34
Jose Luis Duran <jlduran@users.noreply.github.com>
2022-12-06 07:41:16 -0300

    Tests: Add eGFRcr tests
    Obtained from:	https://www.kidney.org/content/national-kidney-foundation-laboratory-engagement-working-group-recommendations-implementing

---
+++ test/models/concerns/derivable_test.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class DerivableTest < ActiveSupport::TestCase
+  setup do
+    @accession = accessions(:egfrcr)
+    @patient = @accession.patient
+    @creatinine = observations(:crtsa)
+    @egfrcr = @accession.results.where(lab_test_id: LabTest.where(code: 'eGFRcr')).take
+  end
+
+  test 'verify appropriate calculation of eGFRcr' do
+    # AGE            Sex            Serum Creatinine    eGFRcr
+    # (years)                       mg/dL               (CKD-EPI 2021)
+    #                                                   mL/min/1.73m2
+    # ------------------------------------------------------------------
+    # <18            Male                               do not calculate
+    # <18            Female                             do not calculate
+    # 18             Male           0.90                127
+    # 18             Male           0.91                125
+    # 18             Female         0.70                128
+    # 18             Female         0.71                126
+    # 90             Male           0.50                97
+    # 90             Male           1.50                44
+    # 90             Female         0.50                89
+    # 90             Female         1.50                33
+    # not available                                     do not calculate
+    #                not available                      do not calculate
+    # Creatinine below or above the measuring interval  do not calculate
+    # (analytical measurement range)
+
+    @patient.update(birthdate: 18.years.ago, gender: 'M')
+    assert_nil @egfrcr.derived_value
+
+    @patient.update(birthdate: 18.years.ago, gender: 'F')
+    assert_nil @egfrcr.derived_value
+
+    @patient.update(birthdate: 19.years.ago, gender: 'M')
+    @creatinine.update(value: 0.90)
+    assert_equal 127, @egfrcr.derived_value
+
+    @patient.update(birthdate: 19.years.ago, gender: 'M')
+    @creatinine.update(value: 0.91)
+    assert_equal 125, @egfrcr.derived_value
+
+    @patient.update(birthdate: 19.years.ago, gender: 'F')
+    @creatinine.update(value: 0.70)
+    assert_equal 128, @egfrcr.derived_value
+
+    @patient.update(birthdate: 19.years.ago, gender: 'F')
+    @creatinine.update(value: 0.71)
+    assert_equal 126, @egfrcr.derived_value
+
+    @patient.update(birthdate: 91.years.ago, gender: 'M')
+    @creatinine.update(value: 0.50)
+    assert_equal 97, @egfrcr.derived_value
+
+    @patient.update(birthdate: 91.years.ago, gender: 'M')
+    @creatinine.update(value: 1.50)
+    assert_equal 44, @egfrcr.derived_value
+
+    @patient.update(birthdate: 91.years.ago, gender: 'F')
+    @creatinine.update(value: 0.50)
+    assert_equal 89, @egfrcr.derived_value
+
+    @patient.update(birthdate: 91.years.ago, gender: 'F')
+    @creatinine.update(value: 1.50)
+    assert_equal 33, @egfrcr.derived_value
+
+    @patient.update(birthdate: nil)
+    assert_nil @egfrcr.derived_value
+
+    @creatinine.update(value: 1.50)
+    assert_nil @egfrcr.derived_value
+  end
+end
