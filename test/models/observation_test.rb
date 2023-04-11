abe1434fcd8a5dafb1addf00881e9dce087a4e7b
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-04-11 20:00:41 +0000

    Test computation result to Infinity

--- test/models/observation_test.rb
+++ test/models/observation_test.rb
@@ -18,4 +18,12 @@ class ObservationTest < ActiveSupport::TestCase
                         value: 'Observation  Value')
     assert_equal 'Observation Value', @observation.value
   end
+
+  test 'value is not in proper decimal notation' do
+    crtsa = observations(:crtsa)
+    egfrcr = observations(:egfrcr)
+    crtsa.update(value: '0,9')
+    assert_equal Float::INFINITY, egfrcr.derived_value
+  end
+
 end
