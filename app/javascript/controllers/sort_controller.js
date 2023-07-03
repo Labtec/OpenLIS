27d9718f73cef430d8b75598ee793f1f14587235
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-07-03 17:38:11 +0000

    Use Sortable.js in the meantime
    While GitHub's task-lists-element is better suited, as it is a purely
    positional-based approach, it has a few bugs that prevents is use at the
    moment.

--- app/javascript/controllers/sort_controller.js
+++ app/javascript/controllers/sort_controller.js
@@ -1,42 +1,41 @@
 import { Controller } from "@hotwired/stimulus"
-import "@github/task-lists-element"
+import Sortable from "sortablejs"
 import { patch } from "@rails/request.js"
 
-// To use this Stimulus controller, create a collection route named "sort"
+// To use this Stimulus controller, create a member route named "sort"
 // and wire it to the appropiate Rails controller.
-// In the views, the task-list container is bound to the Stimulus controller,
-// a URL value is attached with the path to the sort action.
+// In the views, the main container is bound to the Stimulus controller,
+// a URL value is attached with the path to the sort action, using ":id"
+// as a placeholder for the sorted element.
+// The items just pass a data-id attribute with its corresponding
+// database ID.
+// Sortable will populate the event with the newIndex value, that is then
+// passed to the Rails controller's sort action.
 // If everything is OK, it will flash the entire container, providing a
 // visual indication.
 export default class extends Controller {
   static classes = [ "success" ]
-  static targets = [ "tasks" ]
   static values = {
     url: String
   }
 
-  async move(event) {
-    const {src, dst} = event.detail
-    const src_list = src[0] + 1
-    const src_item = src[1] + 1
-    const dst_list = dst[0] + 1
-    let dst_item
-    if (src[1] < dst[1]) {
-      dst_item = dst[1]
-    } else {
-      dst_item = dst[1] + 1
-    }
-    let data = new FormData()
-    data.append("src_list", src_list)
-    data.append("src_item", src_item)
-    data.append("dst_list", dst_list)
-    data.append("dst_item", dst_item)
+  connect() {
+    this.sortable = Sortable.create(this.element, {
+      animation: 150,
+      handle: ".handle",
+      onEnd: this.end.bind(this)
+    })
+  }
 
-    const response = await patch(this.urlValue, { body: data })
+  async end(event) {
+    let id = event.item.dataset.id
+    let data = new FormData()
+    data.append("position", event.newIndex)
+    const response = await patch(this.urlValue.replace(":id", id), { body: data })
     if (response.ok) {
-      this.tasksTarget.parentElement.classList.add(this.successClass)
+      this.element.classList.add(this.successClass)
       setTimeout(() => {
-        this.tasksTarget.parentElement.classList.remove(this.successClass)
+        this.element.classList.remove(this.successClass)
       }, 400)
     }
   }
