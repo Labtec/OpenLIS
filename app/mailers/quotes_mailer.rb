5445bc58b896676001fb24fb2c60405c346dad28
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-09-04 18:35:19 +0000

    Include quote version in email

--- app/mailers/quotes_mailer.rb
+++ app/mailers/quotes_mailer.rb
@@ -3,7 +3,17 @@
 class QuotesMailer < ApplicationMailer
   def email_quote(quote, pdf)
     @quote = quote
-    attachments["#{@quote.serial_number}.pdf"] = pdf.render if pdf
-    mail(to: @quote.email, subject: t('.subject', quote_number: @quote.serial_number))
+    attachments["#{serial_number}.pdf"] = pdf.render if pdf
+    mail(to: @quote.email, subject: t('.subject', quote_number: serial_number))
+  end
+
+  private
+
+  def serial_number
+    if @quote.version_number
+      "#{@quote.serial_number}-#{@quote.version_number}"
+    else
+      @quote.serial_number.to_s
+    end
   end
 end
