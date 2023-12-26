956aced63c401599447ec6b6313b2603fa613a51
Jose Luis Duran <jlduran@users.noreply.github.com>
2023-12-26 20:15:27 +0000

    helpers: Remove unused methods

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -4,13 +4,4 @@ module LayoutHelper
   def title(page_title, show_title = true)
     content_for(:title, page_title.to_s) if show_title
   end
-
-  def stylesheet(*args)
-    content_for(:head) { stylesheet_link_tag(*args.map(&:to_s)) }
-  end
-
-  def javascript(*args)
-    args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
-    content_for(:head) { javascript_include_tag(*args) }
-  end
 end
