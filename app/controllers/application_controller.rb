617e18313277329a7b7e3f54296a4d8417762277
Jose Luis Duran <jlduran@users.noreply.github.com>
2024-08-13 20:25:39 +0000

    Allow old browsers temporarily
    Eventually cap with specific versions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
   protect_from_forgery with: :exception
 
   rescue_from ActionController::RoutingError, with: :not_found
