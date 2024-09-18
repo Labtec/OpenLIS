6578aecc143fc69af8249febb2ce55433cbbf2bc
Jose Luis Duran <jlduran@users.noreply.github.com>
2024-09-18 15:58:16 +0000

    RuboCop: Style/StringLiteralsInInterpolation

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -8,7 +8,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
       browser: :remote,
-      url: "http://#{ENV.fetch("SELENIUM_HOST", nil)}:4444"
+      url: "http://#{ENV.fetch('SELENIUM_HOST', nil)}:4444"
     }
   else
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
