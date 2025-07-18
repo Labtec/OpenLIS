256508ca97f9a6879a4d3c66a6cbe3b02c5414b7
Jose Luis Duran <jlduran@users.noreply.github.com>
2025-07-18 18:34:31 +0000

    propshaft: Add Subresource Integrity (SRI)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,5 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+Rails.application.config.assets.integrity_hash_algorithm = "sha384"
